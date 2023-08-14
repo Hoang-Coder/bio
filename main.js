ab2ae39a90314a89f99818c4aa486ec7f905406e
Hoang-Coder <chukyanh01@gmail.com>
2023-08-14 23:03:49 +0700

    source

---
+++ main.js
@@ -0,0 +1,25 @@
+let direct = (value) => {
+    if (value == 1) {window.location = 'profile.html'}
+    else if (value == 2) {window.location = 'https://github.com/Hoang-Coder'}
+    else if (value == 3) {window.location = 'https://www.facebook.com/anh.phannhat.50702'}
+    else if (value == 4) {window.location = 'https://m.me/anh.phannhat.50702'}
+    else if (value == 5) {window.location = 'https://www.instagram.com/phannhat9497/'}
+    else if (value == 6) {window.location = 'mail:chukyanh01@gmai.com'}
+    else if (value == 7) {window.location = 'phone:0922628889'}
+    else if (value == 8) {window.location = 'https://zalo.me/0922628889'}
+}
+setTimeout(
+    function() {
+        document.getElementById("computer").style.opacity = 1
+        document.getElementById("computer").style.transform = "translateY(0)"
+    },
+    1000
+)
+setTimeout(function() {document.getElementsByTagName("li")[0].style.transform = "translateX(0)"},1200)
+setTimeout(function() {document.getElementsByTagName("li")[1].style.transform = "translateX(0)"},1280)
+setTimeout(function() {document.getElementsByTagName("li")[2].style.transform = "translateX(0)"},1360)
+setTimeout(function() {document.getElementsByTagName("li")[3].style.transform = "translateX(0)"},1440)
+setTimeout(function() {document.getElementsByTagName("li")[4].style.transform = "translateX(0)"},1520)
+setTimeout(function() {document.getElementsByTagName("li")[5].style.transform = "translateX(0)"},1600)
+setTimeout(function() {document.getElementsByTagName("li")[6].style.transform = "translateX(0)"},1680)
+setTimeout(function() {document.getElementsByTagName("li")[7].style.transform = "translateX(0)"},1760)
